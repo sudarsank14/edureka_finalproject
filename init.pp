eb55a34ace119daeaf201bb45df3f130f2a42dfe
Martin1196155 <46367679+Martin1196155@users.noreply.github.com>
2020-06-07 16:07:51 +0530

    Create init.pp

---
+++ init.pp
@@ -0,0 +1,57 @@
+Class: deploy
+# ===========================
+#
+# Full description of class deploy here.
+#
+# Parameters
+# ----------
+#
+# Document parameters here.
+#
+# * `sample parameter`
+# Explanation of what this parameter affects and what it defaults to.
+# e.g. "Specify one or more upstream ntp servers as an array."
+#
+# Variables
+# ----------
+#
+# Here you should define a list of variables that this module would require.
+#
+# * `sample variable`
+#  Explanation of how this variable affects the function of this class and if
+#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#  External Node Classifier as a comma separated list of hostnames." (Note,
+#  global variables should be avoided in favor of class parameters as
+#  of Puppet 2.6.)
+#
+# Examples
+# --------
+#
+# @example
+#    class { 'deploy':
+#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    }
+#
+# Authors
+# -------
+#
+# Author Name <author@domain.com>
+#
+# Copyright
+# ---------
+#
+# Copyright 2020 Your name here, unless otherwise noted.
+#
+class deploy {
+        package{'docker':
+                ensure => 'present',
+        }
+
+        service{'docker':
+                ensure => 'running',
+                enable => 'true',
+        }
+        docker::image{'martin1051/myapp':
+                image_tag => 'latest'
+        }
+}
